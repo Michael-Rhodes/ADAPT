da340f3ddca01bd02c192edeb2a4c07f91b8bf55
Michael-Rhodes <mjr2563@rit.edu>
2018-10-10 12:49:09 -0400

    Update setup-ubuntu-script.sh

--- Setup-Environment/setup-ubuntu-script.sh
+++ Setup-Environment/setup-ubuntu-script.sh
@@ -3,13 +3,28 @@ sudo apt update
 sudo apt upgrade
 
 echo "installing tools: [ Git, Vim, Docker, & Tmux]"
-sudo apt install git vim docker tmux
+sudo apt install -y git vim tmux
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo "Checking Docker installation..."
+docker run hello-world
 
 echo "A reboot should be run after this script"
 
 echo "###"
 echo "Cloning ADAPT repo"
 git clone https://github.com/Michael-Rhodes/ADAPT.git
+sudo sysctl -w vm.max_map_count=262144
 
 echo "Installing Docker-Compose from Docker website.."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -20,6 +35,6 @@ sudo curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/com
 "Refreshing bash config"
 source ~/.bash_profile
 
-"Checking the Installation..."
+echo "Checking the Installation..."
 docker-compose --version
 
