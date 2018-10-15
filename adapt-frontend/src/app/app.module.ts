49c6bdc04cc315221e9509eedfb1ee4ca28c53f4
tanner-g <8582756+tanner-g@users.noreply.github.com>
2018-10-15 14:39:53 -0400

    tracking new table files

--- adapt-frontend/src/app/app.module.ts
+++ adapt-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogTableComponent } from './log-table/log-table.component';
+import { LogTableRowComponent } from './log-table-row/log-table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogTableComponent,
+    LogTableRowComponent
   ],
   imports: [
     BrowserModule
