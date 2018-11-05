305329c586f0c309c276f832d314ca46cfc894aa
tanner-g <8582756+tanner-g@users.noreply.github.com>
2018-11-04 22:17:28 -0500

    added csv to json script, saving work on log webpage

---
+++ adapt-frontend/scripts/csv_to_json.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+csv_file = open('testdata.csv', 'r')
+json_file = open('testdata.json', 'w')
+
+fieldnames = ("APT Group Name", "Matching Events", "Total Events", "% of Events", "Matching TTPs", "Available TTPs", "% of TTPs", "Coverage", "% of Coverage", "Final Value")
+
+reader = csv.DictReader(csv_file, fieldnames)
+for row in reader:
+    json.dump(row, json_file)
+    json_file.write('\n')
+
+
+
--- adapt-frontend/src/app/app-routing.module.ts
+++ adapt-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LogsComponent } from './logs/logs.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
+  { path: 'logs', component: LogsComponent},
   {path: '**', redirectTo: '/home'}
 ]; 
 
